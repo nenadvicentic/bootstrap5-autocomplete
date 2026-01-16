34704c0587eba5053ce0e099f8dbe3a5a8e6253c
Nenad Vićentić <vicentic@immounited.com>
2026-01-16 10:58:56 +0100

    Sync `d.ts` with current source file.

--- types/autocomplete.d.ts
+++ types/autocomplete.d.ts
@@ -233,7 +233,7 @@ declare class Autocomplete {
      * @param {Partial<Config>} config
      */
     private _configure;
-    _config: any;
+    _config: Config;
     /**
      * @private
      */
@@ -248,6 +248,11 @@ declare class Autocomplete {
      * @param {MouseEvent} e
      */
     onclick(e: MouseEvent): void;
+    /**
+     * @param {InputEvent} e
+     * @returns {void}
+     */
+    onbeforeinput(e: InputEvent): void;
     /**
      * @param {InputEvent} e
      * @returns {void}
